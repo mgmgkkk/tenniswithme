7d3403b4068742cca805e9000befc5ac13d31cce
mgmgkkk <alsrudk1@gmail.com>
2026-04-10 13:26:59 +0900

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -204,10 +204,10 @@ const buttonStyle = {
 export default function App() {
   const [tab, setTab] = useState("input");
   const [players, setPlayers] = useState([
-    { name: "민경", start: "16:00", end: "20:00" }
+    { name: "민경", start: "19:00", end: "22:00" }
   ]);
   const [schedule, setSchedule] = useState([
-    { time: "16:00", courts: 1 }
+    { time: "19:00", courts: 1 }
   ]);
   const [result, setResult] = useState([]);
   const [stats, setStats] = useState({});
